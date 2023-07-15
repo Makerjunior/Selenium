da9b92be18e4fe68bf0d795bb748bc3f3ca29632
Makerjunior <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>
2023-07-14 21:22:08 -0300

    end

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Selenium
+
+
+Este código representa um conjunto de testes usando Selenium WebDriver para automatizar interações em uma página do GitHub. Aqui está uma visão geral das principais funcionalidades:
+
+1. O código usa as bibliotecas Selenium e NUnit para escrever testes automatizados.
+2. Ao definir a classe `Tests`, é criada uma instância do `IWebDriver` (ChromeDriver) para controlar o navegador Chrome.
+3. Dentro do método `[SetUp] Setup()`, o driver é inicializado, navegando para a URL especificada (https://github.com/Makerjunior), maximizando a janela do navegador e definindo um tempo de espera implícito de 5 segundos.
+4. O método `[Test] Test()` contém o código para obter informações da página do GitHub, como o número de repositórios públicos, nome de usuário, número de estrelas e número de seguidores.
+5. Os métodos privados `GetNumberOfPublicRepositories()`, `GetUsername()`, `GetStarsCount()` e `GetFollowersCount()` localizam os elementos desejados na página usando XPath e retornam as informações relevantes.
+6. O método `PrintIfNotEmpty()` verifica se uma informação não está vazia e a imprime no console.
+7. O método `ScrollPage()` usa o `IJavaScriptExecutor` para rolar a página para uma posição específica (coordenadas X e Y).
+8. O método `CaptureScreenshot()` captura uma screenshot da página atual, salva-a como um objeto `Screenshot` e a armazena em uma pasta chamada "Prints", criada no diretório atual. A screenshot é salva com um nome único baseado na data e hora de captura.
+9. O método `[TearDown] TearDown()` é executado após cada teste e decide se deve encerrar o driver do navegador ou não, com base na variável `driverQuit`.
+
+Em resumo, este código automatiza a navegação em uma página do GitHub, obtendo informações relevantes e capturando screenshots. Pode ser usado para testar e verificar informações específicas em diferentes perfis de usuário do GitHub.
+
--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿// Importando as bibliotecas necessárias
+using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System;
@@ -12,69 +13,110 @@ namespace Selenium
         public IWebDriver driver;
         public bool driverQuit = true;
 
-
+        // Método de configuração para executar antes de cada teste
         [SetUp]
         public void Setup()
         {
+            // Inicializa o driver do Chrome
             driver = new ChromeDriver();
 
+            // Navega para o URL especificado
             driver.Navigate().GoToUrl("https://github.com/Makerjunior");
+
+            // Maximiza a janela do navegador
             driver.Manage().Window.Maximize();
+
+            // Define um tempo de espera implícito de 5 segundos
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+
+            // Define driverQuit como falso, indicando que o driver não deve ser encerrado após cada teste
             driverQuit = false;
         }
 
+        // Método de teste
         [Test]
         public void Test()
         {
+            // Localiza e obtém informações da página do GitHub
 
+            // Obtém o número de repositórios públicos
+            var numR = GetNumberOfPublicRepositories();
+
+            // Obtém o nome de usuário
+            var userName = GetUsername();
+
+            // Obtém o número de estrelas
+            var magStars = GetStarsCount();
+
+            // Obtém o número de seguidores
+            var msgFollowers = GetFollowersCount();
+
+            // Imprime as informações obtidas se os elementos existirem e não estiverem vazios
+            PrintIfNotEmpty(userName);
+            PrintIfNotEmpty(magStars);
+            PrintIfNotEmpty(msgFollowers);
+            PrintIfNotEmpty(numR);
+
+            // Role a página para uma posição específica
+            ScrollPage(0, 720);
+
+            // Capture uma screenshot da página
+            CaptureScreenshot();
+
+            // Indica que o teste passou com sucesso
+            Assert.Pass();
+        }
+
+        // Método para obter o número de repositórios públicos
+        private string GetNumberOfPublicRepositories()
+        {
             var labelRes = "/html/body/div[1]/div[4]/main/div[1]/div/div/div[2]/div/nav/a[2]/span";
             var resultado = driver.FindElement(By.XPath(labelRes));
-            var numR = "Numero de repositorios publicos :" + resultado.Text;
+            return "Numero de repositorios publicos: " + resultado.Text;
+        }
 
+        // Método para obter o nome de usuário
+        private string GetUsername()
+        {
             var usernameXp = "/html/body/div[1]/div[4]/main/div[2]/div/div[1]/div/div[2]/div[1]/div[2]/h1/span[1]";
             var user = driver.FindElement(By.XPath(usernameXp));
-            var userName = "Usuario :" + user.Text;
+            return "Usuario: " + user.Text;
+        }
 
-
+        // Método para obter o número de estrelas
+        private string GetStarsCount()
+        {
             var stars = "/html/body/div[1]/div[4]/main/div[1]/div/div/div[2]/div/nav/a[5]/span";
             var starsNum = driver.FindElement(By.XPath(stars));
-            var magStars = "Estrelas :" + starsNum.Text;
+            return "Estrelas: " + starsNum.Text;
+        }
 
-
+        // Método para obter o número de seguidores
+        private string GetFollowersCount()
+        {
             var followersXp = "/html/body/div[1]/div[4]/main/div[2]/div/div[1]/div/div[2]/div[3]/div[2]/div[2]/div/a[1]/span";
             var followersNum = driver.FindElement(By.XPath(followersXp));
-            var msgFollowers = "Followers :" + followersNum.Text;
+            return "Followers: " + followersNum.Text;
+        }
 
-            if ((user != null) && (user.Text != " "))
+        // Método para imprimir uma informação se ela não estiver vazia
+        private void PrintIfNotEmpty(string info)
+        {
+            if (!string.IsNullOrWhiteSpace(info))
             {
-                Console.WriteLine(userName);
-
+                Console.WriteLine(info);
             }
-            if ((starsNum != null) && (starsNum.Text != " "))
-            {
-                Console.WriteLine(magStars);
+        }
 
-            }
-            // 
-            if ((followersNum != null) && (followersNum.Text != " "))
-            {
-                Console.WriteLine(msgFollowers);
-
-            }
-
-            if ((resultado != null) && (resultado.Text != " "))
-            {
-                Console.WriteLine(numR);
-
-            }
-
-            // Role a p�gina para a posi��o desejada (x, y) usando JavaScript Executor
-            int scrollX = 0;
-            int scrollY = 720;
+        // Método para rolar a página para uma posição específica
+        private void ScrollPage(int scrollX, int scrollY)
+        {
             ((IJavaScriptExecutor)driver).ExecuteScript($"window.scrollTo({scrollX}, {scrollY});");
+        }
 
-            // Capture a screenshot como um objeto do tipo Screenshot
+        // Método para capturar uma screenshot da página atual
+        private void CaptureScreenshot()
+        {
             Screenshot screenshot = ((ITakesScreenshot)driver).GetScreenshot();
             string dateTimeFormat = "yyyyMMdd_HHmmss";
             string fileName = "screenshot_" + DateTime.Now.ToString(dateTimeFormat) + ".png";
@@ -82,29 +124,32 @@ namespace Selenium
             string folderPath = Path.Combine(currentDirectory, "Prints");
 
 
+            // Cria a pasta "Prints" se ela não existir
             if (!Directory.Exists(folderPath))
             {
                 Directory.CreateDirectory(folderPath);
             }
 
+            // Combina o caminho da pasta com o nome do arquivo
             string path = Path.Combine(folderPath, fileName);
-            screenshot.SaveAsFile(path, ScreenshotImageFormat.Png);
-            // Os Screenshot s�o salvos na pasta bootselenium\bootselenium\bin\Debug\net5.0\Prints
 
-            Assert.Pass();
+            // Salva a screenshot no diretório especificado
+            screenshot.SaveAsFile(path, ScreenshotImageFormat.Png);
         }
 
-
-
+        // Método de limpeza para executar após cada teste
         [TearDown]
         public void TearDown()
         {
-            driver.Quit();
-
+            // Verifica se o driver deve ser encerrado
             if (driverQuit)
             {
-                driver.Quit();
+               
+                driver.Dispose();
             }
+            driver.Quit();
         }
     }
+
 }
+
